06d4873b089991ded01a003eae69eab2834f5809
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2024-12-14 15:19:26 +0000

    useEffect Counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-// import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
-// import HooksCounter from "./components/HooksCounter";
-import UseStateWithObjects from "./components/UseStateWithObjects";
+import UseEffectCounter from "./components/UseEffectCounter";
 
 function App() {
   return <div className="App">
-      {/* <StatefulGreetingWithPrevState />
-      <HooksCounter /> */}
-      <UseStateWithObjects />
+      <UseEffectCounter />
   </div>;
 }
 
